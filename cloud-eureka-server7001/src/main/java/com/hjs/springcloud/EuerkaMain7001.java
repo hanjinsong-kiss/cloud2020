340133dd77eefc0e7a3734c6e267c5611c08609b
hankiss <1449815363@qq.com>
2020-09-16 11:37:27 +0800

    eureka

--- cloud-eureka-server7001/src/main/java/com/hjs/springcloud/EuerkaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/hjs/springcloud/EuerkaMain7001.java
@@ -2,12 +2,14 @@ package com.hjs.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderApplication9000 {
+@EnableEurekaServer
+public class EuerkaMain7001 {
+
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication9000.class,args);
+        SpringApplication.run(EuerkaMain7001.class, args);
     }
+
 }
