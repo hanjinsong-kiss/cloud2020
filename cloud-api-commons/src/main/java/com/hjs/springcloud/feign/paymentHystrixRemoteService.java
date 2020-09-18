8c81467cac323dd79eeabf6fcc25b904d99664d8
hankiss <1449815363@qq.com>
2020-09-18 16:45:15 +0800

    Hystrix 熔断器使用默认的方法

---
+++ cloud-api-commons/src/main/java/com/hjs/springcloud/feign/paymentHystrixRemoteService.java
@@ -0,0 +1,16 @@
+package com.hjs.springcloud.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("payment")
+//@Component
+public interface paymentHystrixRemoteService {
+
+
+    @GetMapping("/payment/get/{id}")
+    public String getById(@PathVariable("id") Long id);
+
+}
--- cloud-comsumer-hystrix-order9001/src/main/java/com/hjs/springcloud/controller/OrderController.java
+++ cloud-comsumer-hystrix-order9001/src/main/java/com/hjs/springcloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.hjs.springcloud.controller;
 
 import com.hjs.springcloud.feign.paymentHystrixRemoteService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.RequiredArgsConstructor;
@@ -10,20 +11,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderController {
 
     private final paymentHystrixRemoteService paymentRemoteService;
 
     @GetMapping("/order/get/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+//    })
+    @HystrixCommand
     public String getOrder(@PathVariable("id") Long id) {
         return paymentRemoteService.getById(id);
     }
 
     public String paymentTimeOutFallbackMethod(Long id) {
         return "调用order失败，请稍后再试";
+    }
+
+    /**
+     * 全局fallback
+     *
+     * @return
+     */
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息,请稍后重试.o(╥﹏╥)o";
     }
 
 }
