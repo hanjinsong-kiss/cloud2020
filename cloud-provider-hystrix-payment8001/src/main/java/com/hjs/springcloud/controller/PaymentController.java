e232d6ac3bf4283c7abab996e8db050bd506c5b4
hankiss <1449815363@qq.com>
2020-09-18 17:27:41 +0800

    Hystrix 熔断器配置参数详解

--- cloud-provider-hystrix-payment8001/src/main/java/com/hjs/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/hjs/springcloud/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.hjs.springcloud.controller;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +26,27 @@ public class PaymentController {
 
     public String payment_TimeOutHandler(Long id) {
         return Thread.currentThread().getName() + "调用超时或失败，请稍后在试试";
+    }
+
+
+    //=====服务熔断
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),// 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),// 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"), // 时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"),// 失败率达到多少后跳闸
+    })
+    @GetMapping("/payment/id/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        if(id < 0) {
+            throw new RuntimeException("******id 不能负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+
+        return Thread.currentThread().getName()+"\t"+"调用成功，流水号: " + serialNumber;
+    }
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id) {
+        return "id 不能负数，请稍后再试，/(ㄒoㄒ)/~~   id: " +id;
     }
 
 }
