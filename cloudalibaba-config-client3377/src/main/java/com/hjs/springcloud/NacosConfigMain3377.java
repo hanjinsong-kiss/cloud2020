a80eb99237c4399d3284a63274d7364b9c20e98f
hankiss <1449815363@qq.com>
2020-09-25 15:01:44 +0800

    nacos config配置中心

---
+++ cloudalibaba-config-client3377/src/main/java/com/hjs/springcloud/NacosConfigMain3377.java
@@ -0,0 +1,19 @@
+package com.hjs.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigMain3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigMain3377.class, args);
+    }
+
+}
+
+
+
+
